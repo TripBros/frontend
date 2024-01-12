1067e6e4ba6f3fe68a23036a21efc55816c18c3a
LeeHyunKyeong <115594388+LeeHyunKyeong@users.noreply.github.com>
2024-01-13 00:57:51 +0900

    [Fix] 파일 경로 수정

--- screens/recommend-screen.tsx
+++ screens/recommend-screen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, StyleSheet, ScrollView } from "react-native";
-import PlaceRecommend from '../components/recommend-screen/place-recommend';
+import PlaceRecommend from '../components/place-recommend';
 
 const RecommendScreen: React.FC = () => {
   return (
